037ce8b63396be83dd8f858b150662643ec7466f
Ariel Eitner <105615462+Ariel-Eitner@users.noreply.github.com>
2023-08-08 04:14:14 -0300

    navBar

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import MyNavbar from "./components/NavBar";
+
+function App() {
+  return (
+    <>
+      <MyNavbar />
+    </>
+  );
+}
+
+export default App;
